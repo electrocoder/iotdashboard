a54cdda8555e175fb3c196936e690772d54d5a7e
electrocoder <electrocoder@gmail.com>
2016-09-02 00:53:14 +0300

    HEROKU = True

--- iotdashboard/wsgi.py
+++ iotdashboard/wsgi.py
@@ -11,8 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iotdashboard.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
