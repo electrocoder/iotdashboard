d260108a0876aa05328883772919071516035ab0
electrocoder <electrocoder@gmail.com>
2016-09-03 19:50:11 +0300

    heroku

--- iotdashboard/wsgi.py
+++ iotdashboard/wsgi.py
@@ -13,6 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iotdashboard.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'iotdashboard.settings'
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
